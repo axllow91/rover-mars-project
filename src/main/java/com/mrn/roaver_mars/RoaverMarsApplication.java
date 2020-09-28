2cf3cdffb19538aeb540b3789fc8267a15dcb371
Marian Dobre <dobremi91@gmail.com>
2020-09-28 12:13:34 +0300

    starting the project

---
+++ src/main/java/com/mrn/roaver_mars/RoaverMarsApplication.java
@@ -0,0 +1,13 @@
+package com.mrn.roaver_mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoaverMarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoaverMarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrn/roaver_mars/RoaverMarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrn.roaver_mars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoaverMarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
